352c45c67e21f7cf1b0b702ee14d3f2235fa0364
Vladyslav Morzhanov <vlad.morzhanov@gmail.com>
2021-08-23 21:52:03 +0300

    Added code for sender

--- internal/apigw/services/apigw.go
+++ internal/apigw/services/apigw.go
@@ -4,17 +4,23 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"strconv"
+	"strings"
 	"time"
 
+	anrpc "github.com/morzhanov/go-realworld/api/rpc/analytics"
 	authrpc "github.com/morzhanov/go-realworld/api/rpc/auth"
-	picturesrpc "github.com/morzhanov/go-realworld/api/rpc/pictures"
-	analyticsdto "github.com/morzhanov/go-realworld/internal/analytics/dto"
-	analyticsmodel "github.com/morzhanov/go-realworld/internal/analytics/models"
+	prpc "github.com/morzhanov/go-realworld/api/rpc/pictures"
+	andto "github.com/morzhanov/go-realworld/internal/analytics/dto"
+	anevents "github.com/morzhanov/go-realworld/internal/analytics/events"
+	anmodel "github.com/morzhanov/go-realworld/internal/analytics/models"
 	authdto "github.com/morzhanov/go-realworld/internal/auth/dto"
+	authevents "github.com/morzhanov/go-realworld/internal/auth/events"
 	"github.com/morzhanov/go-realworld/internal/common/eventlistener"
 	"github.com/morzhanov/go-realworld/internal/common/sender"
-	picturedto "github.com/morzhanov/go-realworld/internal/pictures/dto"
-	picturemodel "github.com/morzhanov/go-realworld/internal/pictures/models"
+	pdto "github.com/morzhanov/go-realworld/internal/pictures/dto"
+	pevents "github.com/morzhanov/go-realworld/internal/pictures/events"
+	pmodel "github.com/morzhanov/go-realworld/internal/pictures/models"
 	uuid "github.com/satori/go.uuid"
 )
 
@@ -32,57 +38,48 @@ type APIGatewayService struct {
 }
 
 func (s *APIGatewayService) handleRest(
-	api string,
-	endpoint string,
+	method string,
+	url string,
 	data interface{},
 	headers *sender.Headers,
 	res interface{},
 ) (err error) {
-	params := s.sender.API[api].Rest[endpoint]
 	b, err := json.Marshal(data)
 	if err != nil {
 		return err
 	}
-	response := s.sender.RestRequest(params.Method, params.Url, b, headers)
+	response := s.sender.RestRequest(method, url, b, headers)
 	return json.Unmarshal(response, &res)
 }
 
-func (s *APIGatewayService) handleEvents(api string, event string, data interface{}, res interface{}) (err error) {
-	uuid := uuid.NewV4().String()
+func (s *APIGatewayService) handleEvents(
+	api string,
+	event string,
+	data string,
+	eventId string,
+	res interface{},
+	wait bool,
+) (err error) {
 	params := s.sender.API[api].Events[event]
-
-	// TODO: move this somewhere to common events package
-	type RequestEvent struct {
-		Uuid string
-		Data string
-	}
-	jsonData, err := json.Marshal(data)
-	if err != nil {
-		return err
-	}
-	eventData := RequestEvent{uuid, string(jsonData)}
-	jsonEventData, err := json.Marshal(&eventData)
-	if err != nil {
-		return err
-	}
 	input := sender.EventsRequestInput{
-		api,
-		params.Event,
-		string(jsonEventData),
+		Service: api,
+		Event:   params.Event,
+		Data:    data,
 	}
 
-	// TODO: we need a listener only if response needed
-	s.sender.EventsRequest(&input)
-	response := make(chan []byte)
-	l := eventlistener.Listener{uuid, response}
-	err = s.eventListener.AddListener(&l)
-	if err != nil {
-		return err
-	}
-	b := <-response
-	err = json.Unmarshal(b, &res)
-	if err != nil {
-		return err
+	if wait {
+		s.sender.EventsRequest(&input)
+		response := make(chan []byte)
+		l := eventlistener.Listener{Uuid: eventId, Response: response}
+		err = s.eventListener.AddListener(&l)
+		if err != nil {
+			return err
+		}
+		b := <-response
+		err = json.Unmarshal(b, &res)
+		if err != nil {
+			return err
+		}
 	}
 	return
 }
@@ -90,7 +87,8 @@ func (s *APIGatewayService) handleEvents(api string, event string, data interfac
 func (s *APIGatewayService) Login(transport Transport, data *authdto.LoginInput) (res *authdto.LoginDto, err error) {
 	switch transport {
 	case rest:
-		err = s.handleRest("auth", "login", data, nil, &res)
+		params := s.sender.API["auth"].Rest["login"]
+		err = s.handleRest(params.Method, params.Url, data, nil, &res)
 	case rpc:
 		client, err := s.sender.GetRpcClient(sender.AuthRpcClient)
 		if err != nil {
@@ -105,9 +103,21 @@ func (s *APIGatewayService) Login(transport Transport, data *authdto.LoginInput)
 		if err != nil {
 			return nil, err
 		}
-		return &authdto.LoginDto{res.AccessToken}, nil
+		return &authdto.LoginDto{AccessToken: res.AccessToken}, nil
 	case events:
-		err = s.handleEvents("auth", "login", data, &res)
+		uuid := uuid.NewV4().String()
+		input := authevents.LoginInput{
+			BaseEventPayload: authevents.BaseEventPayload{EventId: uuid},
+			LoginInput: authdto.LoginInput{
+				Username: data.Username,
+				Password: data.Password,
+			},
+		}
+		json, err := json.Marshal(input)
+		if err != nil {
+			return nil, err
+		}
+		err = s.handleEvents("auth", "login", string(json), uuid, &res, true)
 	default:
 		return nil, fmt.Errorf("Wrong transport type")
 	}
@@ -117,7 +127,8 @@ func (s *APIGatewayService) Login(transport Transport, data *authdto.LoginInput)
 func (s *APIGatewayService) Signup(transport Transport, data *authdto.SignupInput) (res *authdto.LoginDto, err error) {
 	switch transport {
 	case rest:
-		err = s.handleRest("auth", "signup", data, nil, &res)
+		params := s.sender.API["auth"].Rest["signup"]
+		err = s.handleRest(params.Method, params.Url, data, nil, &res)
 	case rpc:
 		client, err := s.sender.GetRpcClient(sender.AuthRpcClient)
 		if err != nil {
@@ -132,36 +143,49 @@ func (s *APIGatewayService) Signup(transport Transport, data *authdto.SignupInpu
 		if err != nil {
 			return nil, err
 		}
-		return &authdto.LoginDto{res.AccessToken}, nil
+		return &authdto.LoginDto{AccessToken: res.AccessToken}, nil
 	case events:
-		err = s.handleEvents("auth", "signup", data, &res)
+		uuid := uuid.NewV4().String()
+		input := authevents.Signup{
+			BaseEventPayload: authevents.BaseEventPayload{EventId: uuid},
+			SignupInput: authdto.SignupInput{
+				Username: data.Username,
+				Password: data.Password,
+			},
+		}
+		json, err := json.Marshal(input)
+		if err != nil {
+			return nil, err
+		}
+		err = s.handleEvents("pictures", "signup", string(json), uuid, &res, true)
 	default:
 		return nil, fmt.Errorf("Wrong transport type")
 	}
 	return
 }
 
-func (s *APIGatewayService) GetPictures(transport Transport, userId string) (res []*picturemodel.Picture, err error) {
+func (s *APIGatewayService) GetPictures(transport Transport, userId string) (res []*pmodel.Picture, err error) {
 	switch transport {
 	case rest:
-		// TODO: we need somehow to inject userId into URL params
-		err = s.handleRest("pictures", "getPictires", nil, nil, &res)
+		params := s.sender.API["pictures"].Rest["getPictures"]
+		url := strings.Replace(params.Url, ":userId", userId, 0)
+		err = s.handleRest(params.Method, url, nil, nil, &res)
 	case rpc:
-		client, err := s.sender.GetRpcClient(sender.AuthRpcClient)
+		client, err := s.sender.GetRpcClient(sender.PicturesRpcClient)
 		if err != nil {
 			return nil, err
 		}
-		authRpcClient := client.(picturesrpc.PicturesClient)
+		picRpcClient := client.(prpc.PicturesClient)
 		ctx, _ := context.WithTimeout(context.Background(), time.Duration(time.Second*5))
-		res, err := authRpcClient.GetUserPictures(ctx, &picturesrpc.GetUserPicturesRequest{
+		res, err := picRpcClient.GetUserPictures(ctx, &prpc.GetUserPicturesRequest{
 			UserId: userId,
 		})
 		if err != nil {
 			return nil, err
 		}
-		pics := make([]*picturemodel.Picture, len(res.Pictures))
+		pics := make([]*pmodel.Picture, len(res.Pictures))
 		for _, pic := range res.Pictures {
-			pics = append(pics, &picturemodel.Picture{
+			pics = append(pics, &pmodel.Picture{
 				ID:     pic.Id,
 				Base64: pic.Base64,
 				Title:  pic.Title,
@@ -170,28 +194,198 @@ func (s *APIGatewayService) GetPictures(transport Transport, userId string) (res
 		}
 		return pics, nil
 	case events:
-		// TODO: we need somehow to inject userId into event
-		err = s.handleEvents("pictures", "getPictires", data, &res)
+		uuid := uuid.NewV4().String()
+		input := pevents.GetUserPicturesInput{
+			BaseEventPayload: pevents.BaseEventPayload{EventId: uuid},
+			UserId:           userId,
+		}
+		json, err := json.Marshal(input)
+		if err != nil {
+			return nil, err
+		}
+		err = s.handleEvents("pictures", "pictures:get", string(json), uuid, &res, true)
 	default:
 		return nil, fmt.Errorf("Wrong transport type")
 	}
 	return
 }
 
-func (s *APIGatewayService) GetPicture(transport Transport, userId string, pictureId string) (res *picturemodel.Picture, err error) {
-	// TODO: send request to pictures service
+func (s *APIGatewayService) GetPicture(transport Transport, userId string, pictureId string) (res *pmodel.Picture, err error) {
+	switch transport {
+	case rest:
+		params := s.sender.API["pictures"].Rest["getPicture"]
+		url := strings.Replace(params.Url, ":userId", userId, 0)
+		url = strings.Replace(url, ":pictureId", pictureId, 0)
+		err = s.handleRest(params.Method, url, nil, nil, &res)
+	case rpc:
+		client, err := s.sender.GetRpcClient(sender.PicturesRpcClient)
+		if err != nil {
+			return nil, err
+		}
+		picRpcClient := client.(prpc.PicturesClient)
+		ctx, _ := context.WithTimeout(context.Background(), time.Duration(time.Second*5))
+		res, err := picRpcClient.GetUserPicture(ctx, &prpc.GetUserPictureRequest{
+			UserId:    userId,
+			PictureId: pictureId,
+		})
+		if err != nil {
+			return nil, err
+		}
+		return &pmodel.Picture{
+			ID:     res.Id,
+			Base64: res.Base64,
+			Title:  res.Title,
+			UserId: res.UserId,
+		}, nil
+	case events:
+		uuid := uuid.NewV4().String()
+		input := pevents.GetUserPictureInput{
+			BaseEventPayload: pevents.BaseEventPayload{EventId: uuid},
+			UserId:           userId,
+			PictireId:        pictureId,
+		}
+		json, err := json.Marshal(input)
+		if err != nil {
+			return nil, err
+		}
+		err = s.handleEvents("pictures", "pictures:get_one", string(json), uuid, &res, true)
+	default:
+		return nil, fmt.Errorf("Wrong transport type")
+	}
+	return
 }
 
-func (s *APIGatewayService) CreatePicture(transport Transport, userId string, data *picturedto.CreatePicturesDto) (res *picturemodel.Picture, err error) {
-	// TODO: send request to pictures service
+func (s *APIGatewayService) CreatePicture(transport Transport, userId string, data *pdto.CreatePicturesDto) (res *pmodel.Picture, err error) {
+	switch transport {
+	case rest:
+		params := s.sender.API["pictures"].Rest["createPicture"]
+		url := strings.Replace(params.Url, ":userId", userId, 0)
+		err = s.handleRest(params.Method, url, nil, nil, &res)
+	case rpc:
+		client, err := s.sender.GetRpcClient(sender.PicturesRpcClient)
+		if err != nil {
+			return nil, err
+		}
+		picRpcClient := client.(prpc.PicturesClient)
+		ctx, _ := context.WithTimeout(context.Background(), time.Duration(time.Second*5))
+		res, err := picRpcClient.CreateUserPicture(ctx, &prpc.CreateUserPictureRequest{
+			UserId: userId,
+			Title:  data.Title,
+			Base64: data.Base64,
+		})
+		if err != nil {
+			return nil, err
+		}
+		return &pmodel.Picture{
+			ID:     res.Id,
+			Base64: res.Base64,
+			Title:  res.Title,
+			UserId: res.UserId,
+		}, nil
+	case events:
+		uuid := uuid.NewV4().String()
+		input := pevents.CreateUserPictureInput{
+			BaseEventPayload: pevents.BaseEventPayload{EventId: uuid},
+			UserId:           userId,
+			CreatePicturesDto: pdto.CreatePicturesDto{
+				Title:  data.Title,
+				Base64: data.Base64,
+			},
+		}
+		json, err := json.Marshal(input)
+		if err != nil {
+			return nil, err
+		}
+		err = s.handleEvents("pictures", "pictures:create", string(json), uuid, &res, true)
+	default:
+		return nil, fmt.Errorf("Wrong transport type")
+	}
+	return
 }
 
 func (s *APIGatewayService) DeletePicture(transport Transport, userId string, pictureId string) error {
-	// TODO: send request to pictures service
+	switch transport {
+	case rest:
+		params := s.sender.API["pictures"].Rest["deletePicture"]
+		url := strings.Replace(params.Url, ":userId", userId, 0)
+		url = strings.Replace(url, ":pictireId", pictureId, 0)
+		err := s.handleRest(params.Method, url, nil, nil, nil)
+		return err
+	case rpc:
+		client, err := s.sender.GetRpcClient(sender.PicturesRpcClient)
+		if err != nil {
+			return err
+		}
+		picRpcClient := client.(prpc.PicturesClient)
+		ctx, _ := context.WithTimeout(context.Background(), time.Duration(time.Second*5))
+		_, err = picRpcClient.DeleteUserPicture(ctx, &prpc.DeleteUserPictureRequest{
+			UserId:    userId,
+			PictureId: pictureId,
+		})
+		return err
+	case events:
+		uuid := uuid.NewV4().String()
+		input := pevents.DeleteUserPictureInput{
+			BaseEventPayload: pevents.BaseEventPayload{EventId: uuid},
+			UserId:           userId,
+			PictireId:        pictureId,
+		}
+		json, err := json.Marshal(input)
+		if err != nil {
+			return err
+		}
+		err = s.handleEvents("pictures", "pictures:create", string(json), uuid, nil, true)
+		return err
+	default:
+		return fmt.Errorf("Wrong transport type")
+	}
 }
 
-func (s *APIGatewayService) GetAnalytics(transport Transport, input *analyticsdto.GetLogsInput) (res *analyticsmodel.AnalyticsEntry, err error) {
-	// TODO: send request to pictures service
+func (s *APIGatewayService) GetAnalytics(transport Transport, input *andto.GetLogsInput) (res *anmodel.AnalyticsEntry, err error) {
+	switch transport {
+	case rest:
+		params := s.sender.API["analytics"].Rest["get"]
+		var url string
+		if input.Offset != 0 {
+			url = strings.Replace(params.Url, ":offset", strconv.Itoa(input.Offset), 0)
+		} else {
+			url = params.Url
+		}
+		err := s.handleRest(params.Method, url, nil, nil, &res)
+		return res, err
+	case rpc:
+		client, err := s.sender.GetRpcClient(sender.AnalyticsRpcClient)
+		if err != nil {
+			return nil, err
+		}
+		picRpcClient := client.(anrpc.AnalyticsClient)
+		ctx, _ := context.WithTimeout(context.Background(), time.Duration(time.Second*5))
+		result, err := picRpcClient.GetLog(ctx, &anrpc.GetLogRequest{
+			Offset: int32(input.Offset),
+		})
+		return &anmodel.AnalyticsEntry{
+			ID:        result.Id,
+			UserID:    result.UserId,
+			Operation: result.Operation,
+			Data:      result.Data,
+		}, err
+	case events:
+		uuid := uuid.NewV4().String()
+		input := anevents.GetLogsInput{
+			BaseEventPayload: anevents.BaseEventPayload{EventId: uuid},
+			GetLogsInput: andto.GetLogsInput{
+				Offset: input.Offset,
+			},
+		}
+		json, err := json.Marshal(input)
+		if err != nil {
+			return nil, err
+		}
+		err = s.handleEvents("pictures", "pictures:create", string(json), uuid, &res, true)
+		return res, err
+	default:
+		return nil, fmt.Errorf("Wrong transport type")
+	}
 }
 
 func NewAPIGatewayService(s *sender.Sender, el *eventlistener.EventListener) *APIGatewayService {
